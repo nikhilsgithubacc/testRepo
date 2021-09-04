a9265307f904ea5ed37a8952423edbf15d870344
nikhilsgithubacc <90109242+nikhilsgithubacc@users.noreply.github.com>
2021-09-04 23:51:54 +0530

    Updated app.js
    Added code for environment port or default 5000.

--- app.js
+++ app.js
@@ -57,4 +57,5 @@ function verifyToken(req, res, next) {
 
 }
 
-app.listen(5000,() => console.log('server started on port 5000'));
+const posrt = process.env.port || 5000
+app.listen(port,() => console.log('server started on port 5000'));
